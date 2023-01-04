586764888b469aa898387e2baff865307389bd2a
nzkaoi <nzkaoi@gmail.com>
2023-01-04 15:40:20 +0900

    bbbb

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-let inputText = document.getElementById("writeText");
-const writeButton = document.getElementById("writeButton");
-const writeLog = document.getElementById("writeLog");
-
 const readButton = document.getElementById("readButton");
 const readLog = document.getElementById("readLog");
 
 readButton.addEventListener("click", () => {
   const ndef = new NDEFReader();
   ndef.scan().then(() => {
-  console.log("Scan started successfully.");
-  ndef.onreadingerror = (event) => {
-    console.log("Error! Cannot read data from the NFC tag. Try a different one?");
-  };
-  ndef.onreading = (event) => {
-    console.log("NDEF message read.");
-  };
-}).catch(error => {
-  console.log(`Error! Scan failed to start: ${error}.`);
-});
+    console.log("Scan started successfully.");
+    ndef.onreadingerror = (event) => {
+      console.log("Error! Cannot read data from the NFC tag. Try a different one?");
+    };
+    ndef.onreading = (event) => {
+      console.log("NDEF message read.");
+    };
+  }).catch(error => {
+    console.log(`Error! Scan failed to start: ${error}.`);
+  });
 });
